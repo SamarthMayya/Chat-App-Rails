6cb09ec27efe43adddc54e2a3aae0bb17962971a
Samarth Mayya <samarthmayya@gmail.com>
2021-03-17 11:28:15 +0530

    First commit

---
+++ app/javascript/channels/conversation_channel.js
@@ -0,0 +1,34 @@
+import consumer from "./consumer"
+
+const conversation = consumer.subscriptions.create("ConversationChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+    var conversation = $('#conversations-list').find("[data-conversation-id='" + data['conversation_id'] + "']");
+    conversation.find('.messages-list').find('ul').append(data['message']);
+
+    var messages_list = conversation.find('.messages-list');
+    var height = messages_list[0].scrollHeight;
+    messages_list.scrollTop(height);
+  },
+
+  speak: function(message) {
+    return this.perform('speak',{
+      message: message
+    });
+  }
+});
+
+  $(document).on('submit','.new_message', function(e) {
+    e.preventDefault();
+    var values = $(this).serializeArray();
+    conversation.speak(values); 
+    $(this).trigger('reset'); 
+});
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,33 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+global.jQuery, global.$ = require("jquery");
+
+(function() {
+  $(document).on('click', '.toggle-window', function(e) {
+    e.preventDefault();
+    var panel = $(this).parent().parent();
+    var messages_list = panel.find('.messages-list');
+
+    panel.find('.panel-body').toggle();
+    panel.attr('class', 'panel panel-default');
+
+    if (panel.find('.panel-body').is(':visible')) {
+      var height = messages_list[0].scrollHeight;
+      messages_list.scrollTop(height);
+    }
+  });
+})();
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+//= require jquery
+//= require jquery_ujs
+//= require_tree
